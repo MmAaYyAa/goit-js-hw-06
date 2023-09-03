73ec8ae7bd806379aae2bc6f6276fa898fd43424
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-09-03 16:55:20 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,31 +1,69 @@
-const list = document.querySelector("#categories");
-// const items = categories.querySelectorAll(".item");
+const list = document.querySelectorAll(".item");
+
+console.log("Number of categories: ", list.length);
+console.log("");
+
+list.forEach((element) => {
+  console.log("Category:", element.firstElementChild.textContent)
+  console.log("Elements:", element.lastElementChild.children.length)
+  console.log("")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const list = document.querySelector("#categories");
+// // const items = categories.querySelectorAll(".item");
+// // console.log(`Number of categories: ${items.length}`);
+
+// let items = [];
+// let item = categories.firstElementChild;
+// while (item) {
+//     items.push(item);
+//     item = item.nextElementSibling;
+// }
 // console.log(`Number of categories: ${items.length}`);
 
-let items = [];
-let item = categories.firstElementChild;
-while (item) {
-    items.push(item);
-    item = item.nextElementSibling;
-}
-console.log(`Number of categories: ${items.length}`);
-
-
-items.forEach(item => {
-  const title = item.querySelector('h2').textContent;
-  let elements = 0;
-  let element = item.querySelector('ul').firstElementChild;
-  while (element) {
-    elements +=1;
-    element = element.nextElementSibling;
-  }
-    console.log(`Category: ${title}
-  Elements: ${elements}`);
-});
 
 // items.forEach(item => {
-//     const title = item.querySelector("h2").textContent;
-//     const elements = item.querySelectorAll("li");
-//     console.log(`Category: ${title} 
-//     Elements: ${elements.length}`)
+//   const title = item.querySelector('h2').textContent;
+//   let elements = 0;
+//   let element = item.querySelector('ul').firstElementChild;
+//   while (element) {
+//     elements +=1;
+//     element = element.nextElementSibling;
+//   }
+//     console.log(`Category: ${title}
+//   Elements: ${elements}`);
 // });
+
+// // items.forEach(item => {
+// //     const title = item.querySelector("h2").textContent;
+// //     const elements = item.querySelectorAll("li");
+// //     console.log(`Category: ${title} 
+// //     Elements: ${elements.length}`)
+// // });

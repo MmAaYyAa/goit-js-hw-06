7031ca05aaa7570df16eca7d2049c4c6ca806c0c
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-09-03 10:10:18 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,31 @@
 const list = document.querySelector("#categories");
-const items = categories.querySelectorAll(".item");
+// const items = categories.querySelectorAll(".item");
+// console.log(`Number of categories: ${items.length}`);
+
+let items = [];
+let item = categories.firstElementChild;
+while (item) {
+    items.push(item);
+    item = item.nextElementSibling;
+}
 console.log(`Number of categories: ${items.length}`);
 
-items.forEach(item => {
-    const title = item.querySelector("h2").textContent;
-    const elements = item.querySelectorAll("li");
-    console.log(`Category: ${title} 
-    Elements: ${elements.length}`)
 
+items.forEach(item => {
+  const title = item.querySelector('h2').textContent;
+  let elements = 0;
+  let element = item.querySelector('ul').firstElementChild;
+  while (element) {
+    elements +=1;
+    element = element.nextElementSibling;
+  }
+    console.log(`Category: ${title}
+  Elements: ${elements}`);
 });
 
+// items.forEach(item => {
+//     const title = item.querySelector("h2").textContent;
+//     const elements = item.querySelectorAll("li");
+//     console.log(`Category: ${title} 
+//     Elements: ${elements.length}`)
+// });

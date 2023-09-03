a74fc2737ad67af3dbfee4655f421051d34f8665
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-09-03 17:07:13 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,6 +7,24 @@ const ingredients = [
   'Condiments',
 ];
 
+const list = document.querySelector("#ingredients");
+const elements = [];
+
+ingredients.forEach(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add("item");
+  elements.push(li);
+});
+
+list.append(...elements);
+
+console.dir(list);
+
+
+
+
+
 
 // const list = document.querySelector("#ingredients");
 // const markup = ingredients.map(ingredient => {
@@ -20,13 +38,13 @@ const ingredients = [
 
 
 
-const list = document.querySelector("#ingredients");
+// const list = document.querySelector("#ingredients");
 
-ingredients.forEach(ingredient => {
-  const li = document.createElement('li');
-  li.textContent = ingredient;
-  li.classList.add("item");
-  list.appendChild(li);
-});
+// ingredients.forEach(ingredient => {
+//   const li = document.createElement('li');
+//   li.textContent = ingredient;
+//   li.classList.add("item");
+//   list.appendChild(li);
+// });
 
-console.dir(list);
+// console.dir(list);
